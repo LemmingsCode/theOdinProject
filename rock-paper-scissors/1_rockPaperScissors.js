a13f1960ef2338aa77e139e0e0f233365ab2243d
LemmingsCode <stevecodecamp@gmail.com>
2021-11-17 14:23:19 +1100

    Created buttons and event listeners for each player choice in the RPS game

--- rock-paper-scissors/1_rockPaperScissors.js
+++ rock-paper-scissors/1_rockPaperScissors.js
@@ -80,4 +80,30 @@ function game() {
     }
 }
 
-game();
+
+const btn = document.querySelector('#btn');
+
+function playerButtonSelection(e) {
+    console.log(e.target.id);
+    console.log(playRound(e.target.id, computerPlay()));
+}
+
+window.addEventListener('click', playerButtonSelection);
+
+// const btn = document.querySelector('#btn');
+// btn.addEventListener('click', function (e) {
+//     // console.log(e.target);
+//     e.target.style.background = 'blue';
+// });
+
+// // buttons is a node list. It looks and acts much like an array.
+// const buttons = document.querySelectorAll('button');
+
+// // we use the .forEach method to iterate through each button
+// buttons.forEach((button) => {
+
+//     // and for each one we add a 'click' listener
+//     button.addEventListener('click', () => {
+//         alert(button.id);
+//     });
+// });
