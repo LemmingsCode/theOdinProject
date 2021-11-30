3b790b23f2b9dab49ff04927a8ec5ea1a441ca7a
LemmingsCode <stevecodecamp@gmail.com>
2021-11-30 14:33:40 +1100

    playing around with eslint

--- whiteboard.js
+++ whiteboard.js
@@ -6,15 +6,37 @@
 log = console.log;
 
 function Car(make, model) {
-        log(this);
-        const props = {
-                make,
-                model,
-        };
-        const info = function () {
-                return this.props;
-        };
+  log(this);
+  const props = {
+    make,
+    model,
+  };
+  const info = function () {
+    return this.props;
+  };
 }
 
 const c1 = Car('Honda', 'Accord');
 const c2 = new Car('Tesla', 'Model 3');
+
+const user = {
+  name: 'John',
+  age: 30,
+  'likes birds': true, // multiword property name must be quoted
+};
+
+user.isAdmin = true;
+
+// console.log(user);
+
+delete user.isAdmin;
+
+console.log(user);
+
+const { name, age, ...test } = {
+  name: 'John',
+  age: 30,
+  'likes birds': true, // multiword property name must be quoted
+};
+
+console.log(test);
